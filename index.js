939abe68dd13ef40623b663de4c8bd078389f019
Daniel Ionita <danielionita88@yahoo.com>
2019-11-17 10:08:49 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+const names = ["Ada", "Drebdan", "Ali"]
+const event = "birthday"
+function writeCards(names, event) {
+    let messages = [];
+    for (let i = 0; i < names.length; i++){
+        messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+    }
+    return messages;
+}
+
+function countDown(integer) {
+    while(integer > 0) {
+        console.log(integer);
+        integer--;
+    }
+    console.log(integer)
+}
